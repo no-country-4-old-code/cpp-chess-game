61d8f0aad5a48c61d916c244e3ee3c6396990982
Koal, Moritz <moritz.koal@draeger.com>
2025-04-08 15:38:46 +0200

    extract behaviour from player

--- src/chaos/chaos.cpp
+++ src/chaos/chaos.cpp
@@ -8,25 +8,51 @@
 enum class Color : std::uint8_t { WHITE, BLACK, COUNT };
 static std::ostream& operator<<(std::ostream& out, Color color);
 
+class PlayerBehaviour {
+   public:
+    virtual void make_move() const = 0;
+    virtual ~PlayerBehaviour()     = 0;
+};
+
+PlayerBehaviour::~PlayerBehaviour() {};
+
+class PlayerBehaviourHuman : public PlayerBehaviour {
+   public:
+    void make_move() const override;
+};
+
+void PlayerBehaviourHuman::make_move() const {
+    std::cout << "<HUMAN>";
+}
+
+class PlayerBehaviourAI : public PlayerBehaviour {
+   public:
+    void make_move() const override;
+};
+
+void PlayerBehaviourAI::make_move() const {
+    std::cout << "<AI>";
+}
 
 class Player {
-    const Color _color;  // NOLINT // copy assignment not needed
+    const Color            _color;      // NOLINT // copy assignment not needed
+    const PlayerBehaviour& _behaviour;  // NOLINT // copy assignment not needed
    public:
-    explicit Player(const Color color) : _color{color} {
+    explicit Player(const Color color, PlayerBehaviour& behaviour)
+        : _color{color}, _behaviour{behaviour} {
     }
-
     [[nodiscard]] Color color() const;
-    void  make_move() const;
+    void                make_move() const;
 };
 
 Color Player::color() const {
     return _color;
-}
+};
 
 void Player::make_move() const {
-    std::cout << "> " << _color << " makes a move\n";
-}
-
+    _behaviour.make_move();
+    std::cout << " > " << this->color() << " makes a move\n";
+};
 
 static const std::array<std::string_view, static_cast<size_t>(Color::COUNT)>
     lookup_color_name{"White", "Black"};
@@ -65,17 +91,20 @@ class IteratorCirulating {
 };
 
 class IteratorTurn {
-    unsigned int _iteration{0};
-    const std::uint8_t _group_size;
-    public:
-    IteratorTurn(const std::uint8_t group_size): _group_size{group_size} {}
+    unsigned int       _iteration{0};
+    const std::uint8_t _group_size;  // NOLINT // copy assignment not needed
 
-    IteratorTurn& operator ++() {
+   public:
+    explicit IteratorTurn(const std::uint8_t group_size)
+        : _group_size{group_size} {
+    }
+
+    IteratorTurn& operator++() {
         ++_iteration;
         return *this;
     }
 
-    [[nodiscard]] unsigned int turn() {
+    [[nodiscard]] unsigned int turn() const {
         return (_iteration / _group_size) + 1;
     }
 };
@@ -85,18 +114,21 @@ class IteratorTurn {
  */
 void run_game() {
     using Players = std::array<Player, 2>;
-    Players players{Player{Color::WHITE}, Player{Color::BLACK}};
+    PlayerBehaviourAI    a{};
+    PlayerBehaviourHuman b{};
+    Players players{Player{Color::WHITE, a}, Player{Color::BLACK, b}};
     IteratorCirulating<Players> player(players.begin(), players.end());
-    IteratorTurn turn_counter{players.size()};
+    IteratorTurn                turn_counter{players.size()};
 
     std::cout << "Start game\n";
     bool is_more_then_one_player_alive{true};
 
     while (is_more_then_one_player_alive) {
-        std::cout << "Turn " << turn_counter.turn();;
+        std::cout << "Turn " << turn_counter.turn();
+        ;
 
         player->make_move();
-        is_more_then_one_player_alive = turn_counter.turn() < 5; // NOLINT
+        is_more_then_one_player_alive = turn_counter.turn() < 5;  // NOLINT
 
         ++turn_counter;
         ++player;
