d45ce32cc008e3082824cea3f2f6ece599dbf373
Koal, Moritz <moritz.koal@draeger.com>
2025-04-16 13:48:12 +0200

    add more unittests to cover left movement (TDD)

--- test/board/test_movements.cpp
+++ test/board/test_movements.cpp
@@ -7,18 +7,49 @@
 #include "board-movements.h"
 
 using namespace board::notation::literal;
+namespace move = board::movements;
+namespace notation = board::notation;
 
 namespace {
-    board::Board default_board{8, 8};
+    const board::Board default_board{8, 8};
+
+    void expect_notation(board::bitmap::Squares position, notation::ChessNotation expected) {
+        notation::ChessNotation end{position, default_board};
+        EXPECT_EQ(end, expected);
+    }
 }
 
-TEST(BoardMovementsHorizontal, Left) {
+// Start -- Test left board movement
+
+TEST(BoardMovementsHorizontal, Left_Valid_b1) {
     auto start        = "b1"_n;
-    auto expected_end = "a1"_n;
-
-    auto end_square =
-        board::movements::left(start.as_squares(default_board), default_board);
-
-    board::notation::ChessNotation end{end_square, default_board};
-    EXPECT_EQ(end, expected_end);
+    auto end_square = move::left(start.as_squares(default_board), default_board);
+    expect_notation(end_square, "a1"_n);
 }
+
+TEST(BoardMovementsHorizontal, Left_Valid_g8) {
+    auto start        = "g8"_n;
+    auto end_square = move::left(start.as_squares(default_board), default_board);
+    expect_notation(end_square, "f8"_n);
+}
+
+TEST(BoardMovementsHorizontal, Left_Invalid_a1) {
+    auto start        = "a1"_n;
+    auto end_square = move::left(start.as_squares(default_board), default_board);
+    EXPECT_EQ(end_square, 0);
+}
+
+TEST(BoardMovementsHorizontal, Left_Invalid_a2) {
+    auto start        = "a2"_n;
+    auto end_square = move::left(start.as_squares(default_board), default_board);
+    EXPECT_EQ(end_square, 0);
+}
+
+TEST(BoardMovementsHorizontal, Left_Invalid_a8) {
+    auto start        = "a8"_n;
+    auto end_square = move::left(start.as_squares(default_board), default_board);
+    EXPECT_EQ(end_square, 0);
+}
+
+// End -- Test left board movement
+// Start -- Test right board movement
