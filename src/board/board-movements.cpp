c93ce1de51005908cca92b2986c487b38e5d9252
Koal, Moritz <moritz.koal@draeger.com>
2025-04-16 14:19:53 +0200

    implement left movement to satisfy unit tests

--- src/board/board-movements.cpp
+++ src/board/board-movements.cpp
@@ -2,9 +2,30 @@
 #include "board.h"
 #include "squares.h"
 
+namespace {
+    board::bitmap::Squares build_left_squares_mask(const board::Board& board);
+}
+
 namespace board::movements {
 
-    bitmap::Squares left(bitmap::Squares position, const Board& /*board*/) {
-        return position >> 1;
+    bitmap::Squares left(bitmap::Squares position, const Board& board) {
+        auto mask = build_left_squares_mask(board); // OPTIMIZE: pull in board later on for speed up
+        if (position & mask) {
+            // We are already as 'left' as we can be.
+            return 0;
+        } else {
+            return position >> 1;
+        }
+    }
+}
+
+namespace {
+    board::bitmap::Squares build_left_squares_mask(const board::Board& board) {
+        // Create a bitmap representing all square on the left edge (a1, a2, a3, ...)
+        board::bitmap::Squares mask = 0;
+        for (int i = 0; i < board.num_of_squares_vertical; i++) {
+            mask |= 1ULL << (board.num_of_squares_horizontal * i);
+        }
+        return mask;
     }
 }
