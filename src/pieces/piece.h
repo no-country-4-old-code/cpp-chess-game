7acc876fe8b79e3b63d0f43bd9053bbed752d196
Koal, Moritz <moritz.koal@draeger.com>
2025-04-16 11:12:16 +0200

    Add piece data structure

---
+++ src/pieces/piece.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "squares.h"
+#include "pieces-color.h"
+
+
+namespace piece {
+    using PieceType = u_int8_t;
+
+    struct Piece {
+        Color color;
+        PieceType type;
+        board::bitmap::Squares position; 
+        
+        board::bitmap::Squares observed; // All "seen" squares including ones blocked by other pieces (including own)
+        board::bitmap::Squares attackable; // All squares the piece might move to (not considering checkmate)
+        board::bitmap::Squares movable; // All squares the piece can move to (considering checkmate)   
+    };
+}
+
