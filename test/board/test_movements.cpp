87eef1398e96c832f2e549ded093245bbf3442be
Koal, Moritz <moritz.koal@draeger.com>
2025-04-16 11:21:04 +0200

    Rename test_board to test_notation and add test_movement draft

---
+++ test/board/test_movements.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include <notation.h>
+#include <squares.h>
+#include <board-movements.h>
+#include <sstream>
+#include <tuple>
+
+TEST(BoardMovementsHorizontal, Left) {
+    board::notation::ChessNotation obj{"a1"};
+}
