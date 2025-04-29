e14c6c69f0c30163ba1b5c8bfdef1f745421b0c7
Koal, Moritz <moritz.koal@draeger.com>
2025-04-29 09:29:24 +0200

    correct rock movement

--- src/pieces/pieces/piece-rock.cpp
+++ src/pieces/pieces/piece-rock.cpp
@@ -1,6 +1,7 @@
 #include "piece-rock.h"
 #include "piece.h"
 #include "board-movements.h"
+#include <array>
 
 namespace piece::pieces::rock
 {
@@ -8,14 +9,34 @@ namespace piece::pieces::rock
 
     void update_observed_and_attackable(piece::Piece& piece, const board::Board &board, sqrs pos_all, sqrs pos_hostile_armies)
     {
-        const auto &position = piece.position;
-        piece.observed =
-            move::left(position, board) |
-            move::up(position, board) |
-            move::right(position, board) |
-            move::down(position, board);
+        const std::array<move::move_func, 4> directions {
+            move::left,
+            move::right,
+            move::up,
+            move::down
+        };
 
-        auto pos_own_pieces = pos_all & ~pos_hostile_armies;
-        piece.attackable = piece.observed & ~pos_own_pieces; // can not attack own pieces
+        piece.observed = 0;
+        piece.attackable = 0;
+
+        for (auto go: directions) {
+            auto current = piece.position;
+            while (current != 0) {
+                current = go(current, board);
+                piece.observed |= current;
+
+                if (current & pos_all) {
+                    if (current & pos_hostile_armies) {
+                        // only pieces of enemies can be attacked
+                        piece.attackable |= current;
+                    }
+                    break;
+                } else {
+                    // squares in sight without a piece can be attacked
+                    piece.attackable |= current;
+                }
+            }
+        }
+        
     }
 }
