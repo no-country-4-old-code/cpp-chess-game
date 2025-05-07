cb8c6daa7595017faa1cd7d19134eedc0dff11e0
Koal, Moritz <moritz.koal@draeger.com>
2025-05-07 12:59:39 +0200

    first idea of update movable

--- src/pieces/piece-api.cpp
+++ src/pieces/piece-api.cpp
@@ -5,7 +5,102 @@
 #include "aggregator-positions.h"
 
 namespace {
+    void update_movable_pieces_of_army(piece::army::Army army, const board::Board& board, piece::aggregator::army_list& army_list) {
+        board::bitmap::Squares under_attack_map = 0x12345; // TODO all attackable from other armies
+        board::bitmap::Squares under_observation_map = 0x12345; // TODO all observale from other armies
+        
+        auto king = army.king();
+        bool is_king_under_attack = king.position & under_attack_map;
+        board::bitmap::Squares positions_of_my_army = 0x123222; // TODO
 
+        if (is_king_under_attack) {
+            int number_of_attackers = 1;
+
+            for (auto i=0; i < army.size(); ++i) {
+                army.pieces[i].movable = 0;
+            }
+
+            king.movable = king.attackable & ~under_observation_map;
+            
+            if (number_of_attackers == 1) {
+                // only pieces which can intercept the threat to the king !
+                piece::Piece* attacker = &army.pieces[1]; //  TODO: find attacker
+                // get fields between attacker and king
+                board::bitmap::Squares interceptable = 1234;
+                for (auto i=0; i < army.size(); ++i) {
+                    auto& piece = army.pieces[i];
+
+                    if (piece.position == king.position) {
+                        continue;
+                    }
+
+                    piece.movable = piece.attackable & interceptable;
+
+                    if (piece.movable) {
+                        // Do is under attack check from below
+                        // piece.position & under_attack_map
+                    }
+                }   
+                
+            }
+
+            
+
+            // pass
+        } else {
+            for (auto i=0; i < army.size(); ++i) {
+                auto& piece = army.pieces[i];
+
+                if (piece.position == king.position) {
+                    king.movable = king.attackable & ~under_observation_map;
+                }
+
+                else if (piece.position & under_attack_map) {
+                    bool does_movement_endanger_king = false;
+                    auto positions_without_piece = positions_of_my_army & ~piece.position;
+                    // check enemy pieces
+                    for (auto& enemy_army: army_list) {
+                        if (enemy_army.color() == army.color()) {
+                            continue; 
+                        }
+
+                        for (auto j=0; j < army.size(); ++j) {
+                            auto& enemy = enemy_army.pieces[j];
+
+                            if(piece.position & enemy.attackable) {
+                                // add quick check if relative position to each other (if king left to piece then attacker has to be right to piece etc.)
+                                
+                                // heavy load calcs:
+                                auto tmp = enemy;
+                                tmp.update_observed_and_attackable(board, positions_without_piece, positions_without_piece);
+
+                                if (king.position & tmp.attackable) {
+                                    does_movement_endanger_king = true;
+                                    break;
+                                }
+                            }
+                        }
+
+                        if(does_movement_endanger_king) {
+                            break;
+                        }
+                    }
+                    
+                    if(does_movement_endanger_king) {
+                        // movement lead to checkmate
+                        piece.movable = 0;
+                    } else {
+                        // free to move
+                        piece.movable = piece.observed;
+                    }
+                }
+                else {
+                    // free to move
+                    piece.movable = piece.observed;
+                }
+            }
+        }
+    }
 
 }
 
