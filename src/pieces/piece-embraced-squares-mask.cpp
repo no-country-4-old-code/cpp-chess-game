e005109924d6400aa77083702fa01d08c7109836
Koal, Moritz <moritz.koal@draeger.com>
2025-05-15 23:21:44 +0200

    rework of piece-embraced part 2

--- src/pieces/piece-embraced-squares-mask.cpp
+++ src/pieces/piece-embraced-squares-mask.cpp
@@ -5,14 +5,15 @@
 
 namespace {
     struct Position {
-        const int horizontal;
-        const int vertical;
-        Position(int h, int v) : horizontal{h}, vertical{v} {};
+        int h;
+        int v;
+        Position(int horizontal, int vertical) : h{horizontal}, v{vertical} {}
     };
 
     inline u_int8_t get_bit_index(board::bitmap::Squares);
     inline Position map_to_position(board::bitmap::Squares, const board::Board&);
     inline bool is_straight_or_diagonal(const Position&, const Position&);
+    inline int sign(int x) { return (x > 0) - (x < 0); }
 }
 
 namespace piece::utils {
@@ -21,26 +22,26 @@ namespace piece::utils {
         if (!pos_bitmap1 || !pos_bitmap2 || pos_bitmap1 == pos_bitmap2)
             return 0;
 
-        auto pos1 = map_to_position(pos_bitmap1, board);
-        auto pos2 = map_to_position(pos_bitmap2, board);
+        const auto pos1 = map_to_position(pos_bitmap1, board);
+        const auto pos2 = map_to_position(pos_bitmap2, board);
 
-        int dx = pos2.horizontal - pos1.horizontal;
-        int dy = pos2.vertical - pos1.vertical;
+        const int dx = pos2.h - pos1.h;
+        const int dy = pos2.v - pos1.v;
 
         if (!is_straight_or_diagonal(pos1, pos2))
             return 0;
 
-        int step_x = (dx == 0 ? 0 : dx / std::abs(dx));
-        int step_y = (dy == 0 ? 0 : dy / std::abs(dy));
+        const int step_x = sign(dx);
+        const int step_y = sign(dy);
 
+        int x = pos1.h + step_x;
+        int y = pos1.v + step_y;
+
+        const int width = board.num_of_squares_horizontal;
         sqrs result = 0;
 
-        int x = pos1.horizontal + step_x;
-        int y = pos1.vertical + step_y;
-
-        while (x != pos2.horizontal || y != pos2.vertical) {
-            int index = y * board.num_of_squares_horizontal + x;
-            result |= sqrs{1ULL << index};
+        while (x != pos2.h || y != pos2.v) {
+            result |= sqrs{1ULL << (y * width + x)};
             x += step_x;
             y += step_y;
         }
@@ -57,14 +58,14 @@ namespace {
     inline Position map_to_position(const board::bitmap::Squares pos, const board::Board& board) {
         assert(std::has_single_bit(pos));
         const u_int8_t bit_index = get_bit_index(pos);
-        const u_int8_t horizontal = bit_index % board.num_of_squares_horizontal;
-        const u_int8_t vertical = bit_index / board.num_of_squares_horizontal;
-        return Position(horizontal, vertical);
+        const int h = bit_index % board.num_of_squares_horizontal;
+        const int v = bit_index / board.num_of_squares_horizontal;
+        return Position(h, v);
     }
 
     inline bool is_straight_or_diagonal(const Position& a, const Position& b) {
-        int dx = std::abs(a.horizontal - b.horizontal);
-        int dy = std::abs(a.vertical - b.vertical);
+        const int dx = std::abs(a.h - b.h);
+        const int dy = std::abs(a.v - b.v);
         return dx == 0 || dy == 0 || dx == dy;
     }
 }
