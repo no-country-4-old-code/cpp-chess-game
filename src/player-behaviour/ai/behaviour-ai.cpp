51c933fd301de81de8483ca224764bec92793d3c
Koal, Moritz <moritz.koal@draeger.com>
2025-05-19 22:57:21 +0200

    correct is_defated routine

--- src/player-behaviour/ai/behaviour-ai.cpp
+++ src/player-behaviour/ai/behaviour-ai.cpp
@@ -142,11 +142,21 @@ Piece PlayerBehaviourAI::get_piece_on_dest(Bitmap  /*dest*/) {
     return {};
 }
 
+int get_number_of_living_pieces(piece::army::Army &army) {
+    int count = 0;
+    for (auto piece: army.pieces) {
+        if (piece.is_alive()) {
+            ++count;
+        }
+    }
+    return count;
+}
+
 bool PlayerBehaviourAI::is_defeated(piece::army::Army &my_army) const {
     auto valid_moves = piece::api::calc_possible_moves(my_army, this->_board, this->_army_list);
     bool are_there_valid_moves = valid_moves.size() > 0;
     bool is_king_alive = my_army.king().is_alive();
-    bool have_multiple_pieces = my_army.size() > 1;
+    bool have_multiple_pieces = get_number_of_living_pieces(my_army) > 1;
     return !(are_there_valid_moves & is_king_alive & have_multiple_pieces);
 }
 
