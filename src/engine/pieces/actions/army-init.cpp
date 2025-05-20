d28c53288bff3f74bab64d1a8d0981b6a5cc1a3a
Koal, Moritz <moritz.koal@draeger.com>
2025-05-20 11:56:23 +0200

    refactor spliting api-piece.cpp

---
+++ src/engine/pieces/actions/army-init.cpp
@@ -0,0 +1,36 @@
+#include "piece-api.h"
+#include "piece.h"
+#include "board.h"
+#include "squares.h"
+
+namespace piece::api
+{
+    void init_army_list(piece::army::army_list &army_list, const board::Board &board)
+    {
+        std::array<board::bitmap::Squares, army::max_num_of_armies> army_positions_lookup;
+        board::bitmap::Squares positions_all = 0;
+
+        // update and calc positions
+        for (auto i = 0; i < army_list.size(); ++i)
+        {
+            army_positions_lookup[i] = 0;
+            for (auto &current : army_list[i].pieces)
+            {
+                army_positions_lookup[i] |= current.position;
+                positions_all |= current.position;
+            }
+        }
+
+        auto idx_army = 0;
+        for (auto &army : army_list)
+        {
+            auto positions_hostile_pieces = positions_all & ~army_positions_lookup[idx_army];
+            ++idx_army;
+
+            for (auto &piece : army.pieces)
+            {
+                piece.update_observed_and_attackable(board, positions_all, positions_hostile_pieces);
+            }
+        }
+    }
+}
--- src/engine/pieces/actions/calc-moves.cpp
+++ src/engine/pieces/actions/calc-moves.cpp
@@ -2,12 +2,10 @@
 #include "piece.h"
 #include "board.h"
 #include "squares.h"
-
 #include "piece-embraced-squares-mask.h"
 
 namespace
 {
-
     bool does_piece_movement_endanger_own_king(const piece::Piece &piece, const board::Board &board, const piece::army::army_list &army_list, const piece::army::Army &my_army, const board::bitmap::Squares positions_of_my_army, const board::bitmap::Squares under_attack_map)
     {
         if (piece.position & under_attack_map) // only matter if piece is under attack at all
@@ -149,82 +147,5 @@ namespace piece::api
             }
         }
         return memory;
-    }
-
-    void move_piece(const board::bitmap::Squares src, const board::bitmap::Squares dest, const board::Board &board, piece::army::army_list &army_list)
-    {
-        assert(std::has_single_bit(src));
-        assert(std::has_single_bit(dest));
-        assert(dest != src);
-
-        std::array<board::bitmap::Squares, army::max_num_of_armies> army_positions_lookup;
-        board::bitmap::Squares positions_all = 0;
-
-        // update and calc positions
-        for (auto i = 0; i < army_list.size(); ++i)
-        {
-            army_positions_lookup[i] = 0;
-            for (auto &current : army_list[i].pieces)
-            {
-                if (current.position == src)
-                {
-                    current.position = dest;
-                }
-
-                else if (current.position == dest)
-                {
-                    current.mark_as_dead();
-                }
-
-                army_positions_lookup[i] |= current.position;
-                positions_all |= current.position;
-            }
-        }
-
-        // update observed and attackable of all affected pieces
-        auto affected_squares = src | dest;
-
-        auto idx_army = 0;
-        for (auto &army : army_list)
-        {
-            auto positions_hostile_pieces = positions_all & ~army_positions_lookup[idx_army];
-            ++idx_army;
-            for (auto &current : army.pieces)
-            {
-                if ((current.observed | current.position) & affected_squares)
-                {
-                    current.update_observed_and_attackable(board, positions_all, positions_hostile_pieces);
-                }
-            }
-        }
-    }
-
-    void init_army_list(piece::army::army_list &army_list, const board::Board &board)
-    {
-        std::array<board::bitmap::Squares, army::max_num_of_armies> army_positions_lookup;
-        board::bitmap::Squares positions_all = 0;
-
-        // update and calc positions
-        for (auto i = 0; i < army_list.size(); ++i)
-        {
-            army_positions_lookup[i] = 0;
-            for (auto &current : army_list[i].pieces)
-            {
-                army_positions_lookup[i] |= current.position;
-                positions_all |= current.position;
-            }
-        }
-
-        auto idx_army = 0;
-        for (auto &army : army_list)
-        {
-            auto positions_hostile_pieces = positions_all & ~army_positions_lookup[idx_army];
-            ++idx_army;
-
-            for (auto &piece : army.pieces)
-            {
-                piece.update_observed_and_attackable(board, positions_all, positions_hostile_pieces);
-            }
-        }
     }
 }
