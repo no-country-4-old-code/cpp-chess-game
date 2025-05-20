4e4b973ca9992d7cc7024efef536f4848c50b238
Koal, Moritz <moritz.koal@draeger.com>
2025-05-20 15:54:34 +0200

    throw away display all pieces from test-utils

--- test/_helper/display.cpp
+++ test/_helper/display.cpp
@@ -36,72 +36,10 @@ namespace
         }
     }
 
-    std::map<piece::PieceType, char> lookup_piece_to_notation{
-        // bin. search is slower then simply index, BUT std::cout not used in time critical context anyway
-        {piece::PieceType::KING, 'K'},
-        {piece::PieceType::ROCK, 'R'},
-        {piece::PieceType::BISHOP, 'B'}};
-
-    std::map<Color, char> lookup_color_to_notation{
-        // bin. search is slower then simply index, BUT std::cout not used in time critical context anyway
-        {Color::WHITE, 'w'},
-        {Color::BLACK, 'b'},
-        {Color::ORANGE, 'o'},
-        {Color::BLUE, 'b'},
-    };
-
 }
 
 namespace test::helper
 {
-
-    void display_all_pieces(const board::Board &board, const piece::army::army_list &army_list)
-    {
-        std::cout << "\nPrint Board\n\n";
-        std::cout << " \t a   b   c   d   e   f   g   h  \n";
-
-        const auto num_of_squares = board.num_of_squares_horizontal * board.num_of_squares_vertical;
-        std::vector<std::array<char, 2>> squares;
-        squares.resize(num_of_squares, {' ', '\0'});
-
-        for (auto &sqr : squares)
-        {
-            sqr[0] = ' ';
-            sqr[1] = ' ';
-        }
-
-        for (auto army : army_list)
-        {
-            for (auto i = 0; i < army.size(); ++i)
-            {
-                const piece::Piece *ptr = &army.pieces[i];
-                if (ptr->is_alive())
-                {
-                    assert(std::has_single_bit(ptr->position));
-                    const auto idx = std::countr_zero(ptr->position);
-                    squares[idx][0] = lookup_piece_to_notation[ptr->type];
-                    squares[idx][1] = lookup_color_to_notation[army.color()];
-                }
-            }
-        }
-
-        u_int row_count = 1;
-        std::cout << row_count << "\t";
-
-        for (auto sqr : squares)
-        {
-            std::cout << " " << sqr[0] << sqr[1] << " ";
-            if (row_count % board.num_of_squares_horizontal == 0)
-            {
-                std::cout << "\n"
-                          << (row_count / bits_per_byte) + 1 << "\t";
-            }
-            std::cout << std::flush;
-            ++row_count;
-        }
-
-        std::cout << "\n";
-    }
 
     void display_bits_on_board(const board::Board &board, board::bitmap::Squares sqrs)
     {
--- test/_helper/test-helper.h
+++ test/_helper/test-helper.h
@@ -4,6 +4,5 @@
 
 namespace test::helper
 {
-    void display_all_pieces(const board::Board &board, const piece::army::army_list &army_list);
     void display_bits_on_board(const board::Board &board, board::bitmap::Squares sqrs);
 }
