b1eaa922ca19036ae48995198ac382139ee5cfdc
Koal, Moritz <moritz.koal@draeger.com>
2025-05-28 11:32:32 +0200

    refactor rock - move method by giving move-method at compile time via template parameter

--- src/engine/pieces/types/pieces/rock.cpp
+++ src/engine/pieces/types/pieces/rock.cpp
@@ -8,50 +8,51 @@
 
 namespace
 {
-    void update(piece::Piece &piece, const board::Board &board, const piece::Positions &);
+    namespace move = board::movements;
+
+    template <move::move_func Move>
+    void move_until_blocked(piece::Piece &piece, const board::Board &board, const piece::Positions &positions) {
+        auto current = piece.position;
+        while (current != 0)
+        {
+            current = Move(current, board);
+            piece.observed |= current;
+
+            if ((current & positions.all_armies))
+            {
+                if ((current & positions.hostile_armies))
+                {
+                    // only pieces of enemies can be attacked
+                    piece.attackable |= current;
+                }
+                break;
+            } 
+            // squares in sight without a piece can be attacked
+            piece.attackable |= current;
+        }
+    }
+
+
+    template <move::move_func... Funcs>
+    void update_template(piece::Piece &piece, const board::Board &board, const piece::Positions &positions) {
+        piece.observed = 0;
+        piece.attackable = 0;
+        (move_until_blocked<Funcs>(piece, board, positions) , ...);
+        piece.movable = piece.attackable;
+    }
+
+
 }
 
 namespace piece
 {
     Piece Rock(board::Board board, board::notation::ChessNotation notation)
     {
-        return {PieceType::ROCK, notation.as_squares(board), ::update};
+        return {
+            PieceType::ROCK,
+            notation.as_squares(board),
+            ::update_template<move::left, move::right, move::up, move::down>
+        };
     }
 }
 
-namespace
-{
-    namespace move = board::movements;
-
-    void update(piece::Piece &piece, const board::Board &board, const piece::Positions &positions)
-    {
-
-        const std::array<move::move_func, 4> directions{move::left, move::right, move::up,
-                                                        move::down};
-
-        piece.observed = 0;
-        piece.attackable = 0;
-
-        for (auto move_fn : directions)
-        {
-            auto current = piece.position;
-            while (current != 0)
-            {
-                current = move_fn(current, board);
-                piece.observed |= current;
-
-                if ((current & positions.all_armies))
-                {
-                    if ((current & positions.hostile_armies))
-                    {
-                        // only pieces of enemies can be attacked
-                        piece.attackable |= current;
-                    }
-                    break;
-                } // squares in sight without a piece can be attacked
-                piece.attackable |= current;
-            }
-        }
-        piece.movable = piece.attackable;
-    }
-} // namespace
