ea56f8badf0f2f4e3592c197a540e49a6f8f7827
Koal, Moritz <moritz.koal@draeger.com>
2025-07-01 15:25:28 +0200

    refactor make-move by extracting functions

--- src/engine/ai/make-move.cpp
+++ src/engine/ai/make-move.cpp
@@ -19,41 +19,54 @@
 #include "score-list.h"
 #include "run-simulation.h"
 
+namespace {
+    inline size_t get_idx(const piece::army::Army &my_army, const piece::army::army_list &army_list);
+    inline void execute_move(const ai::simulation::SimulationResult& result, piece::army::Army &my_army, piece::army::army_list &army_list, const board::Board& board);
+}
+
 
 ai::Move ChessAI::make_move(piece::army::Army &my_army)
 {
-
-
-    // find my index
-    auto army_index = 0;
-    for (auto army : this->_army_list)
-    {
-        if (army.color() == my_army.color())
-        {
-            break;
-        }
-        ++army_index;
-    }
-
+    auto army_index = get_idx(my_army, this->_army_list);
     auto result = ai::simulation::run_simulation(this->_board, this->_army_list, army_index);
-
-    if (result.move.src)
-    {
-        piece::api::move_piece(result.move.src, result.move.dest, this->_board, this->_army_list);
-
-        if (result.move.extra.src)
-        {
-            piece::api::move_piece(result.move.extra.src, result.move.extra.dest, this->_board, this->_army_list);
-        }
-
-        std::cout << "Moved from " << board::notation::ChessNotation(result.move.src, this->_board) << " to " << board::notation::ChessNotation(result.move.dest, this->_board) << std::endl;
-    }
-    else
-    {
-        my_army.mark_as_defeated();
-        std::cout << "# CHECKMATE - No further move possible !!!\n"
-                  << std::flush;
-    }
+    execute_move(result, my_army, this->_army_list, this->_board);
 
     return result.move;
+}
+
+
+namespace {
+
+    inline size_t get_idx(const piece::army::Army &my_army, const piece::army::army_list &army_list) {
+        size_t army_index = 0;
+        for (auto army : army_list)
+        {
+            if (army.color() == my_army.color())
+            {
+                break;
+            }
+            ++army_index;
+        }
+        return army_index;
+    }
+
+    inline void execute_move(const ai::simulation::SimulationResult& result, piece::army::Army &my_army, piece::army::army_list &army_list, const board::Board& board) {
+        if (result.move.src)
+        {
+            piece::api::move_piece(result.move.src, result.move.dest, board, army_list);
+
+            if (result.move.extra.src)
+            {
+                piece::api::move_piece(result.move.extra.src, result.move.extra.dest, board, army_list);
+            }
+
+            std::cout << "Moved from " << board::notation::ChessNotation(result.move.src, board) << " to " << board::notation::ChessNotation(result.move.dest, board) << std::endl;
+        }
+        else
+        {
+            my_army.mark_as_defeated();
+            std::cout << "# CHECKMATE - No further move possible !!!\n"
+                    << std::flush;
+        }
+    }
 }
