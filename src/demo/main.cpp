1eb2f5dec9fc60d2a5e98273605d4f8f5fde9185
Koal, Moritz <moritz.koal@draeger.com>
2025-07-03 08:50:38 +0200

    change demo game

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -19,24 +19,26 @@ int main() {
     using namespace piece;                     // NOLINT - accepted in top-level function
     using namespace board::notation::literal;  // NOLINT - using namespace for literals is pretty ok
 
-    board::Board const board{8, 8};
+    board::Board const board{4, 8};  // TODO: Why are those convert ?!?!?
+    const auto up = MoveDirection::UP;
+    const auto down = MoveDirection::DOWN;
+
 
     army::army_list army_list = {
-        army::Army{Color::BLUE, {King(board, "a1"_n), Rock(board, "e4"_n), Bishop(board, "b6"_n)}},
-        army::Army{Color::WHITE,
-                   {
-                       King(board, "h2"_n),
-                       Rock(board, "f8"_n),
-                       Rock(board, "g7"_n),
-                   }},
-        army::Army{Color::ORANGE, {King(board, "d8"_n), Rock(board, "d7"_n)}}, army::Army{}};
+        army::Army{Color::WHITE, {
+            Rock(board, "a1"_n), Bishop(board, "b1"_n), King(board, "c1"_n), Knight(board, "d1"_n),
+            Pawn(board, "a2"_n, up), Pawn(board, "b2"_n, up), Pawn(board, "c2"_n, up), Pawn(board, "d2"_n, up) 
+        }},
+        army::Army{Color::BLACK, {
+            Pawn(board, "a7"_n, down), Pawn(board, "b7"_n, down), Pawn(board, "c7"_n, down), Pawn(board, "d7"_n, down), 
+            Rock(board, "a8"_n), Bishop(board, "b8"_n), King(board, "c8"_n), Knight(board, "d8"_n)
+        }},army::Army{}, army::Army{}};
 
     ai::ChessAI ai{army_list, board};
 
     demo::game::PlayerGroup group{
         Player{&ai, &army_list[0]},
-        Player{&ai, &army_list[1]},
-        Player{&ai, &army_list[2]},
+        Player{&ai, &army_list[1]}
     };
 
     demo::game::run(group, board, army_list);
